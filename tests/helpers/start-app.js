34c5fd7a37d627ffd8a28402fdc58600e74f1f80
Robert Wagner <rwwagner90@gmail.com>
2017-03-17 20:21:56 -0400

    Fix tests

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,13 +1,13 @@
-import { assign } from  'ember-platform';
-import run from 'ember-runloop';
+import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
+const { merge, run } = Ember;
 
 export default function startApp(attrs) {
-  let attributes = assign({}, config.APP);
-  attributes = assign(attributes, attrs); // use defaults, but you can override;
+  let attributes = merge({}, config.APP);
+  attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  run(() => {
+  return run(() => {
     let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
