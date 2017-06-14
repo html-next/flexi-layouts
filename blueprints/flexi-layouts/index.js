e61fb65c0ddf3d93e8261fea4f88aea2530eccd6
Robert Wagner <rwwagner90@gmail.com>
2017-06-13 22:22:44 -0400

    Remove target

--- blueprints/flexi-layouts/index.js
+++ blueprints/flexi-layouts/index.js
@@ -5,7 +5,6 @@ module.exports = {
   afterInstall: function(options) {
     return this.addAddonToProject({
       name: '@html-next/flexi-config',
-      target: '~2.0.0-rc.1',
       blueprintOptions: {
         save: options.save
       }
